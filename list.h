f0ba35a5fceade87ddb5ee84999b338b8863409d
Colwelljj <col18046@byui.edu>
2021-10-15 14:16:48 -0600

    changes

--- list.h
+++ list.h
@@ -15,7 +15,7 @@
  *        List         : A class that represents a List
  *        ListIterator : An iterator through List
  * Author
- *    Alexander Dohms, Stephen Costigan, Shaun Crook
+ *    Alexander Dohms, Stephen Costigan, Shaun Crook, Jonathan Colwell
  ************************************************************************/
 
 #pragma once
